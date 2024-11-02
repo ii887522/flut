688afdea4111f1ffd93307e9544724c5ed45344b
ii887522 <ii887522@gmail.com>
2024-11-03 00:00:13 +0800

    Initial commit

---
+++ examples/worm/main.rs
@@ -0,0 +1,4 @@
+#![cfg_attr(all(not(debug_assertions), windows), windows_subsystem = "windows")]
+#![deny(elided_lifetimes_in_paths)]
+
+const fn main() {}
---
+++ src/lib.rs
@@ -0,0 +1,1 @@
+#![deny(elided_lifetimes_in_paths)]
